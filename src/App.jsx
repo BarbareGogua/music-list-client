e63f0d6118766a88ba612060d9588ebe6a02eee8
barbare gogua <goguabarbare4@gmail.com>
2026-01-31 14:16:52 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
+import AddMusic from './pages/AddMusic';
+import Landing from './pages/Landing';
+import NavBar from './components/NavBar';
+
+const App = () => {
+  return (
+    <BrowserRouter> 
+    <div>
+      <NavBar></NavBar>
+      <div>
+        <div>
+          <Routes>
+            <Route path="/" element={ <Landing/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/add" element={<AddMusic/>}/>
+          </Routes>
+        </div>
+      </div>
+    </div>
+    </BrowserRouter>
+  )
+};
+
+export default App
---
+++ src/pages/AddMusic.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddMusic = () => {
+  return (
+    <div>
+      hello music
+    </div>
+  );
+};
+
+export default AddMusic;
